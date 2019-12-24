b29d969d904d049a0cfa87f33985e095f7a54d1c
WitchHanna <58846076+WitchHanna@users.noreply.github.com>
2019-12-24 13:51:40 +0300

    Add files via upload
    functions created

---
+++ storaged/functions.sql
@@ -0,0 +1,53 @@
+USE ALLIDB;
+
+DELIMITER ;;
+
+CREATE FUNCTION rehabilitated_cases()
+RETURNS DOUBLE
+BEGIN
+	DECLARE percent DOUBLE DEFAULT 0;
+	DECLARE cases_num INT;
+    DECLARE rehabilitated_num INT;
+    
+    SELECT COUNT(*) INTO cases_num FROM cases;
+    SELECT COUNT(*) INTO rehabilitated_num FROM rehabilitation;
+    
+    SET percent = (rehabilitated_num / cases_num) * 100;
+    
+    RETURN percent;
+END;;
+
+CREATE FUNCTION rehabilitated_people()
+RETURNS DOUBLE
+BEGIN
+	DECLARE percent DOUBLE DEFAULT 0;
+	DECLARE person_num INT;
+    DECLARE rehabilitated_num INT;
+    
+    SELECT COUNT(DISTINCT person_id) INTO person_num FROM cases;
+    
+    SELECT COUNT(DISTINCT cases.person_id) INTO rehabilitated_num
+    FROM cases
+    INNER JOIN rehabilitation ON rehabilitation.case_id = cases.case_id;
+    
+    SET percent = (rehabilitated_num / person_num) * 100;
+    
+    RETURN percent;
+END;;
+
+CREATE FUNCTION shot_percent()
+RETURNS DOUBLE
+BEGIN
+	DECLARE percent DOUBLE DEFAULT 0;
+	DECLARE person_num INT;
+    DECLARE shot_num INT;
+    
+    SELECT COUNT(*) INTO shot_num FROM shot;
+    SELECT COUNT(DISTINCT person_id) INTO person_num FROM cases;
+    
+    SET percent = (shot_num / person_num) * 100;
+    
+    RETURN percent;
+END;;
+
+DELIMITER ;
